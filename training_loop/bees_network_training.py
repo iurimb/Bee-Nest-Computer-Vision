65c5df5e8b54ff3829eed7c0f0921e220f6f6beb
Iuri Barcat <138903202+iurimb@users.noreply.github.com>
2024-08-28 13:16:31 -0300

    Update bees_network_training.py

--- training_loop/bees_network_training.py
+++ training_loop/bees_network_training.py
@@ -13,32 +13,14 @@ epochs_list = [5]
 
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
-data = 'C:\Users\noz\Documents\Clutch\Abelhas\ANNOTATIONS_ABELHAS'
+
+data = 'YOUR_DATA.YAML_FILE_PATH'
+#if you want to do a sequence of training with different epoch numbers
 for i in range(len(epochs_list)):
     print(f"We'll be training for {epochs_list[i]} epochs")
-    #Train the model using the 'coco128.yaml' dataset for 3 epochs
+   
     results = model.train(data=data, epochs=epochs_list[i])
 
     # Evaluate the model's performance on the validation set
 results = model.val()
 
-# Perform object detection on an image using the model
-#results = model('C:/Users/L04578/Documents/Mexilhao/Fotos_Microscopio_2023.10.11/2023.11.10_10.10.52_image_154.png')
-
-
-# results = model.predict(source="folder", show=True) # Display preds. Accepts all YOLO predict arguments
-
-# # from PIL
-# im1 = Image.open("bus.jpg")
-# results = model.predict(source=im1, save=True)  # save plotted images
-
-# from ndarray
-
-# print("--------------TESTING--------------------")
-# im2 = cv2.imread('C:/Users/L04578/Documents/Mexilhao/Fotos_Microscopio_2023.10.11/2023.11.10_10.10.52_image_255.png')
-# results = model.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
-
-# from list of PIL/ndarray
-#results = model.predict(source=[ im2])
-# Export the model to ONNX format
-# success = model.export(format='onnx')
