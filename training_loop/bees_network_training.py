9cc00a41ffa063f46b1152b3533c1deefb90c79d
Iuri Barcat <138903202+iurimb@users.noreply.github.com>
2024-08-28 13:16:58 -0300

    Update bees_network_training.py

--- training_loop/bees_network_training.py
+++ training_loop/bees_network_training.py
@@ -2,16 +2,10 @@ from ultralytics import YOLO
 import cv2
 from PIL import Image
 
-# # Create a new YOLO model from scratch
-# #model = YOLO('yolov8n.yaml')
-# model = YOLO('yolov8n-seg.yaml')
-# # Load a pretrained YOLO model (recommended for training)
-# #model = YOLO('yolov8n.pt')
-# model = YOLO('yolov8n-seg.pt') 
-#model = YOLO('yolov8n-seg.yaml').load('yolov8n-seg.pt')  # build from YAML and transfer weights
+
 epochs_list = [5]
 
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights (from ultralytics)
 
 
 data = 'YOUR_DATA.YAML_FILE_PATH'
